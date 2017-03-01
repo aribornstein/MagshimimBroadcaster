ca8212589c45c6e183dd21a4cf64dd89bc8e2677
FroPeX <shoafi100@gmail.com>
2017-03-01 20:17:56 +0200

    Add files via upload

---
+++ ISCREENRECORDER/Core/BitmapScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace Core
+{
+    public class BitmapScreen: IScreen
+    {
+        //implememtation of a screen as a bitmap image.
+        public object screenRepresentation
+        {
+            get { return screen; }
+            set { screen = value as Bitmap; }
+
+        }
+
+        Bitmap screen;
+
+        
+    }
+}
